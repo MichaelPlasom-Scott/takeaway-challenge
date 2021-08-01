6ff0ff2d116a7e89c19719a73169833f13e04ca7
Michael PS <michael.plasomscott@hotmail.co.uk>
2021-08-01 17:35:26 +0100

    adds food selections to array

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -1,16 +1,29 @@
 class Takeaway
-  attr_reader :menu
+  attr_reader :menu, :food_array
 
   def initialize
-    @menu = [{food: 'Pizza', price: 1},
-      {food: 'Curry', price: 2},
-      {food: 'Noodles', price: 3
-      }]
+    @menu = {
+      "Pizza" => 1,
+      "Curry" => 2,
+      "Noodles" => 3,
+    }
+    @food_array = []
   end
 
   def show_menu
     @menu
   end
 
-end
+  def pick_pizza
+    @food_array << "Pizza"
+  end
 
+  def pick_curry
+    @food_array << "Curry"
+  end
+
+  def pick_noodles
+    @food_array << "Noodles"
+  end
+
+end
--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -4,24 +4,29 @@ describe Takeaway do
   # As a customer
   # So that I can check if I want to order something
   # I would like to see a list of dishes with prices
-
-
-  it "shows menu options" do
-    takeaway = Takeaway.new
-    expect(takeaway.show_menu).to eq([{food: 'Pizza', price: 1},
-    {food: 'Curry', price: 2},
-    {food: 'Noodles', price: 3
-    }])
-
+  describe "#show_menu" do
+    it "shows menu options" do
+      takeaway = Takeaway.new
+      expect(takeaway.show_menu).to eq subject.menu
+    end
   end
-
   # As a customer
   # So that I can order the meal I want
   # I would like to be able to select 
   #   some number of several available dishes
-
-  
-
-
-
+  describe "#pick meal" do
+    it "puts pizza in array" do
+      expect(subject.pick_pizza).to eq subject.food_array
+    end
+    it "puts curry in array" do
+      expect(subject.pick_curry).to eq subject.food_array
+    end
+    it "puts noodles in array" do
+      expect(subject.pick_noodles).to eq subject.food_array
+    end
+  end
+  # As a customer
+  # So that I can verify that my order is correct
+  # I would like to check that the total I have been given 
+  #   matches the sum of the various dishes in my order
 end
